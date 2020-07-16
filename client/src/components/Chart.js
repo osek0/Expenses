351a9a9ea51131ebe68523abb234115c78ab2899
Oskar Dyngus <dyngusoskar@gmail.com>
2020-07-17 00:48:14 +0200

    split data to multiple charts

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -7,11 +7,17 @@ class expChart extends React.Component {
     super(props);
     this.state = {
       labels: [],
-      data: []
+      food: [],
+      cleaning: [],
+      hygiene: [],
+      hobby: [],
+      fun: [],
+      clothes: [],
+      other: []
     };
   }
 
-  processData = ({data}) => {
+  getAmounts = (data, category) => {
     const amounts = new Array(32);
 
     data.map( doc => {
@@ -30,9 +36,54 @@ class expChart extends React.Component {
       }
     });
 
-    // console.log(data);
+    this.setState({ [category] : amounts.reverse()});
+  }
 
-    this.setState({data: amounts.reverse()});
+  processData = ({data}) => {
+
+    console.log(data);
+
+    const food = [];
+    const cleaning = [];
+    const hygiene = [];
+    const hobby = [];
+    const fun = [];
+    const clothes = [];
+    const other = [];
+
+    data.map( doc => {
+      switch (doc.category) {
+        case 'Jedzenie':
+          food.push(doc);
+          break;
+        case 'Środki czystości':
+          cleaning.push(doc);
+          break;
+        case 'Higiena':
+          hygiene.push(doc);
+          break;
+        case 'Hobby/Sport':
+          hobby.push(doc);
+          break;
+        case 'Rozrywka':
+          fun.push(doc);
+          break;
+        case 'Odzież':
+          clothes.push(doc);
+          break;
+        case 'Inne':
+          other.push(doc);
+          break;
+      }
+
+      this.getAmounts(food, 'food');
+      this.getAmounts(cleaning, 'cleaning');
+      this.getAmounts(hygiene, 'hygiene');
+      this.getAmounts(hobby, 'hobby');
+      this.getAmounts(fun, 'fun');
+      this.getAmounts(clothes, 'clothes');
+      this.getAmounts(other, 'other');
+    });
   }
 
   getDateArray = () => {
@@ -55,11 +106,27 @@ class expChart extends React.Component {
 
   renderColors = (red, green, blue, alpha) => {
     const colors = Array(32);
+
     for(let i=0; i < 32; i++) {
       colors[i] = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
     }
-    console.log(colors);
+
     return colors;
+  }
+
+  renderCharts = (chartName, amounts, colors) => {
+    const chart = {
+      label: chartName,
+      data: amounts,
+      backgroundColor: this.renderColors(...colors[0]),
+      borderColor: this.renderColors(...colors[1]),
+      borderWidth: 1,
+      //borderColor: '#777',
+      //hoverBorderColor: '#000',
+      hoverBorderWidth: 2
+    };
+
+    return chart;
   }
 
   drawChart = () => {
@@ -68,45 +135,13 @@ class expChart extends React.Component {
       type: 'line',
       data: {
         labels: this.state.labels,
-        datasets: [{
-          label: 'Amount',
-          data: this.state.data,
-          backgroundColor: this.renderColors(255, 99, 132, 0.2),
-          //   'rgba(255, 99, 132, 0.2)',
-          //   'rgba(54, 162, 235, 0.2)',
-          //   'rgba(255, 206, 86, 0.2)',
-          //   'rgba(75, 192, 192, 0.2)',
-          //   'rgba(153, 102, 255, 0.2)',
-          //   'rgba(255, 159, 64, 0.2)'
-          borderColor: this.renderColors(255, 99, 132, 1),
-          //   'rgba(255, 99, 132, 1)',
-          //   'rgba(54, 162, 235, 1)',
-          //   'rgba(255, 206, 86, 1)',
-          //   'rgba(75, 192, 192, 1)',
-          //   'rgba(153, 102, 255, 1)',
-          //   'rgba(255, 159, 64, 1)'
-          borderWidth: 1,
-          //borderColor: '#777',
-          //hoverBorderColor: '#000',
-          hoverBorderWidth: 2
-
-        },
-        // {
-        //   label: 'Amount2',
-        //   data: [24, 32, 16, 63],
-        //   backgroundColor: [
-        //     'rgba(54, 162, 235, 0.2)',
-        //     'rgba(54, 162, 235, 0.2)'
-        //   ],
-        //   borderColor: [
-        //     'rgba(54, 162, 235, 1)',
-        //     'rgba(54, 162, 235, 1)'
-        //   ],
-        //   borderWidth: 1,
-        //   //borderColor: '#777',
-        //   //hoverBorderColor: '#000',
-        //   hoverBorderWidth: 2
-        // }
+        datasets: [
+          this.renderCharts('Jedzenie', this.state.food, [[255, 99, 132, 0.2], [255, 99, 132, 1]]),
+          this.renderCharts('Sprzątanie', this.state.cleaning, [[37, 95, 133, 0.2], [37, 95, 133, 1]]),
+          this.renderCharts('Higiena', this.state.hygiene, [[249, 185, 195, 0.2], [249, 185, 195, 1]]),
+          this.renderCharts('Hobby/Sport', this.state.hobby, [[0, 135, 76, 0.2], [0, 135, 76, 1]]),
+          this.renderCharts('Ubrania', this.state.clothes, [[255, 200, 87, 0.2], [255, 200, 87, 1]]),
+          this.renderCharts('Inne', this.state.other, [[255, 255, 255, 0.2], [255, 255, 255, 1]])
         ]
       },
       options: {
